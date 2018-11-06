55a5849077ef1033365730075375bd7606161791
sidneisimeao <sidneisimmon@gmail.com>
2018-11-06 11:28:32 -0200

    Ajustes diversos

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import Helmet from 'react-helmet';
 import Posts from 'components/posts/Posts';
 
+const canonical = "https://www.sidneisimmon.com/blog";
+const description = "Blog sobre React, Javascript, SEO e Tecnología geral. Acesse o Blog e fique atualizado";
+
 export default () => (
-    <div className="column is-centered">
-        <Posts />
-    </div>
+    <Fragment>
+        <Helmet>
+            <meta property="og:type" content="website" />
+            <meta name="description" content={description} />
+            <meta property="og:locale" content="pt_BR" />
+            <meta property="og:url" content={`${canonical}`} />
+            <link rel="canonical" href={`${canonical}`} />
+            <meta property="og:title" content="Página inicial, postagens mais recentes" />
+            <meta property="og:site_name" content="Sidnei Simmon" />
+            <meta property="og:description" content={description} />
+        </Helmet>
+        <div className="column is-centered">
+            <Posts />
+        </div>
+    </Fragment>
 );
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Post from 'components/posts/Post';
 import withPosts from 'hocs/withPosts';
+import Helmet from 'react-helmet';
 
-const postBySlug = ( posts,  slug) => posts.find( post => post.slug === slug );
+const postBySlug = (posts, slug) => posts.find(post => post.slug === slug);
 
-const Posts = ({ posts,  match }) => {
-  const post = postBySlug(posts, match.params.slug );
-  return !!post ? <Post key={post.slug} {...post} /> : []    
+const Posts = ({ posts, match }) => {
+  const post = postBySlug(posts, match.params.slug);
+  return !!post ?
+    <Fragment>
+      <Helmet title={`${post.title}`} />
+      <Post key={post.slug} {...post} />
+    </Fragment>
+    : []
 }
 
 export default withPosts(Posts);
